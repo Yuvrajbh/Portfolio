c9d4e740e4a74ad1e91c08d6e717c9e646cefa01
Yuvrajbh <yuvrajbhalla05@gmail.com>
2024-04-10 01:23:35 +0530

    Skills

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About/About';
 import Head from './components/Head/Head';
 import Home from './components/Home/Home';
+import Skills from './components/Skills/Skills';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Head/>
      <main className="main">
       <Home/>
-      <About/>
+      <About></About>
+      <Skills/>
      </main>
     </div>
   );
